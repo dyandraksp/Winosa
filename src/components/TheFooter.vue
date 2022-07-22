7d9954d59abde50e0a98945bddd05934787d17df
Dyandra Ayu KSP <dyandraksp@gmail.com>
2022-07-22 16:04:00 +0700

    about + footer

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,176 +1,66 @@
 <template>
   <!-- Footer -->
-  <MDBFooter bg="dark" :text="['center', 'white']">
+  <MDBFooter bg="dark" :text="['white']">
     <!-- Grid container -->
     <MDBContainer class="p-4">
-      <!-- Section: Social media -->
-      <section class="mb-4">
-        <!-- Facebook -->
-        <a
-          class="btn btn-outline-light btn-floating m-1"
-          href="#!"
-          role="button"
-          ><MDBIcon iconStyle="fab" icon="facebook-f"
-        /></a>
-        <!-- Twitter -->
-        <a
-          class="btn btn-outline-light btn-floating m-1"
-          href="#!"
-          role="button"
-          ><MDBIcon iconStyle="fab" icon="twitter"
-        /></a>
-        <!-- Google -->
-        <a
-          class="btn btn-outline-light btn-floating m-1"
-          href="#!"
-          role="button"
-          ><MDBIcon iconStyle="fab" icon="google"
-        /></a>
-        <!-- Instagram -->
-        <a
-          class="btn btn-outline-light btn-floating m-1"
-          href="#!"
-          role="button"
-          ><MDBIcon iconStyle="fab" icon="instagram"
-        /></a>
-        <!-- Linkedin -->
-        <a
-          class="btn btn-outline-light btn-floating m-1"
-          href="#!"
-          role="button"
-          ><MDBIcon iconStyle="fab" icon="linkedin-in"
-        /></a>
-        <!-- Github -->
-        <a
-          class="btn btn-outline-light btn-floating m-1"
-          href="#!"
-          role="button"
-          ><MDBIcon iconStyle="fab" icon="github"></MDBIcon
-        ></a>
-      </section>
-      <!-- Section: Social media -->
-      <!-- Section: Form -->
-      <section class="">
-        <form action="">
-          <!--Grid row-->
-          <MDBRow center class="d-flex">
-            <!--Grid column-->
-            <MDBCol auto>
-              <p class="pt-2">
-                <strong>Sign up for our newsletter</strong>
-              </p>
-            </MDBCol>
-            <!--Grid column-->
-            <!--Grid column-->
-            <MDBCol md="5" col="12">
-              <!-- Email input -->
-              <MDBInput
-                white
-                type="email"
-                id="form5Example2"
-                label="Email address"
-                wrapperClass="mb-4"
-              />
-            </MDBCol>
-            <!--Grid column-->
-            <!--Grid column-->
-            <MDBCol auto>
-              <!-- Submit button -->
-              <MDBBtn outline="light" class="mb-4"> Subscribe </MDBBtn>
-            </MDBCol>
-            <!--Grid column-->
-          </MDBRow>
-          <!--Grid row-->
-        </form>
-      </section>
-      <!-- Section: Form -->
-      <!-- Section: Text -->
-      <section class="mb-4">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt
-          distinctio earum repellat quaerat voluptatibus placeat nam,
-          commodi optio pariatur est quia magnam eum harum corrupti
-          dicta, aliquam sequi voluptate quas.
-        </p>
-      </section>
-      <!-- Section: Text -->
-      <!-- Section: Links -->
       <section class="">
         <!--Grid row-->
         <MDBRow>
           <!--Grid column-->
           <MDBCol lg="3" md="6" class="mb-4 mb-md-0">
-            <h5 class="text-uppercase">Links</h5>
+            <h5 class="text-uppercase">WINOSA</h5>
+            <p style="text-align: left;">Menjadi salah satu perusahaan penyedia jasa
+              konsultan dan pengembangan bisnis di bidang IT
+              yang terpercaya untuk memberikan solusi yang
+              inovatif dan mampu bersaing dalam skala
+              nasional dan internasional</p>
+          </MDBCol>
+          <!--Grid column-->
+          <!--Grid column-->
+          <MDBCol lg="3" md="6" class="mb-4 mb-md-0">
+            <h5 class="text-uppercase">HOME</h5>
             <ul class="list-unstyled mb-0">
               <li>
-                <a href="#!" class="text-white">Link 1</a>
+                <a href="#!" class="text-white">About</a>
               </li>
               <li>
-                <a href="#!" class="text-white">Link 2</a>
+                <a href="#!" class="text-white">Partners</a>
               </li>
               <li>
-                <a href="#!" class="text-white">Link 3</a>
+                <a href="#!" class="text-white">Career</a>
               </li>
               <li>
-                <a href="#!" class="text-white">Link 4</a>
+                <a href="#!" class="text-white">Contact</a>
               </li>
             </ul>
           </MDBCol>
           <!--Grid column-->
           <!--Grid column-->
           <MDBCol lg="3" md="6" class="mb-4 mb-md-0">
-            <h5 class="text-uppercase">Links</h5>
+            <h5 class="text-uppercase">PORTFOLIO</h5>
             <ul class="list-unstyled mb-0">
               <li>
-                <a href="#!" class="text-white">Link 1</a>
+                <a href="#!" class="text-white">Xdemia</a>
               </li>
               <li>
-                <a href="#!" class="text-white">Link 2</a>
+                <a href="#!" class="text-white">Truexess</a>
               </li>
               <li>
-                <a href="#!" class="text-white">Link 3</a>
+                <a href="#!" class="text-white">Crowdpie</a>
               </li>
               <li>
-                <a href="#!" class="text-white">Link 4</a>
+                <a href="#!" class="text-white">Novimix</a>
               </li>
             </ul>
           </MDBCol>
           <!--Grid column-->
           <!--Grid column-->
           <MDBCol lg="3" md="6" class="mb-4 mb-md-0">
-            <h5 class="text-uppercase">Links</h5>
+            <h5 class="text-uppercase">CONTACT</h5>
             <ul class="list-unstyled mb-0">
-              <li>
-                <a href="#!" class="text-white">Link 1</a>
-              </li>
-              <li>
-                <a href="#!" class="text-white">Link 2</a>
-              </li>
-              <li>
-                <a href="#!" class="text-white">Link 3</a>
-              </li>
-              <li>
-                <a href="#!" class="text-white">Link 4</a>
-              </li>
-            </ul>
-          </MDBCol>
-          <!--Grid column-->
-          <!--Grid column-->
-          <MDBCol lg="3" md="6" class="mb-4 mb-md-0">
-            <h5 class="text-uppercase">Links</h5>
-            <ul class="list-unstyled mb-0">
-              <li>
-                <a href="#!" class="text-white">Link 1</a>
-              </li>
-              <li>
-                <a href="#!" class="text-white">Link 2</a>
-              </li>
-              <li>
-                <a href="#!" class="text-white">Link 3</a>
-              </li>
-              <li>
-                <a href="#!" class="text-white">Link 4</a>
-              </li>
+              <li>alamat</li>
+              <li>info@winosa.id</li>
+              <li>085839261747</li>
             </ul>
           </MDBCol>
           <!--Grid column-->
@@ -195,15 +85,22 @@
   <!-- Footer -->
 </template>
 <script>
-import { MDBFooter, MDBContainer, MDBRow, MDBCol, MDBIcon, MDBBtn } from 'mdb-vue-ui-kit'
+import { MDBFooter, MDBContainer, MDBRow, MDBCol } from 'mdb-vue-ui-kit'
 export default {
   components: {
     MDBFooter,
     MDBContainer,
     MDBRow,
-    MDBCol,
-    MDBIcon,
-    MDBBtn
+    MDBCol
   }
 }
 </script>
+<style>
+li{
+  text-align: left;
+}
+h5{
+  color: #FDC03A;
+  text-align: left;
+}
+</style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,3 +1,34 @@
 <template>
-    <h1>About</h1>
+  <div class="">
+    <div
+      id="carouselExampleSlidesOnly"
+      class="carousel slide"
+      data-ride="carousel"
+    >
+      <div class="carousel-inner">
+        <div
+          class="carousel-item"
+          v-for="(banner, idx) in banners"
+          :class="{ active: idx == 0 }"
+          :key="idx"
+        >
+          <img :src="banner" alt="" class="img-fluid" />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+export default {
+  name: 'app-head',
+  data () {
+    return {
+      banners: [
+        'https://images.unsplash.com/photo-1606857521015-7f9fcf423740?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80'
+      ]
+    }
+  },
+  methods: {}
+}
+</script>
