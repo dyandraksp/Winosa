83b3e45cca83c6d5aae7d003823921de9bc6b0c5
delia vergarefta <deliaverga1001@gmail.com>
2022-07-25 11:48:45 +0700

    update portfolio

--- src/views/PortfolioView.vue
+++ src/views/PortfolioView.vue
@@ -1,3 +1,71 @@
 <template>
-    <h1>Portfolio</h1>
+<html>
+   <div class="">
+    <div
+      id="carouselExampleSlidesOnly"
+      class="carousel slide"
+      data-ride="carousel"
+    >
+      <div class="carousel-inner">
+        <div
+          class="carousel-item"
+          v-for="(banner, idx) in banners"
+          :class="{ active: idx == 0 }"
+          :key="idx"
+        >
+          <img :src="banner" alt="" class="img-fluid" style="width: 100%; height: 570px; object-fit: cover;"/>
+        </div>
+      </div>
+    </div>
+  </div>
+<!-- Slideshow container -->
+<div class="slideshow-container">
+
+  <!-- Full-width images with number and caption text -->
+  <div class="mySlides fade">
+    <div class="numbertext">1 / 3</div>
+    <img :src="img1" style="width:100%">
+    <div class="text">Caption Text</div>
+  </div>
+  <div class="mySlides fade">
+    <div class="numbertext">2 / 3</div>
+    <img :src="img2" style="width:100%">
+    <div class="text">Caption Two</div>
+  </div>
+  <div class="mySlides fade">
+    <div class="numbertext">3 / 3</div>
+    <img :src="img3" style="width:100%">
+    <div class="text">Caption Three</div>
+  </div>
+
+  <!-- Next and previous buttons -->
+  <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+  <a class="next" onclick="plusSlides(1)">&#10095;</a>
+</div>
+<br>
+
+<!-- The dots/circles -->
+<div style="text-align:center">
+  <span class="dot" onclick="currentSlide(1)"></span>
+  <span class="dot" onclick="currentSlide(2)"></span>
+  <span class="dot" onclick="currentSlide(3)"></span>
+</div>
+</html>
 </template>
+<script>
+export default {
+  name: 'app-head',
+  data () {
+    return {
+      img1: [
+        'https://images.unsplash.com/photo-1606857521015-7f9fcf423740?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80'
+      ],
+      banners: [
+        'https://images.unsplash.com/photo-1432821596592-e2c18b78144f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+      ]
+    }
+  },
+  methods: {}
+}
+
+</script>
