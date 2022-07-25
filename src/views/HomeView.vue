57b6ccd286ed30a3deac10d2de7cca5dede42fb8
delia vergarefta <deliaverga1001@gmail.com>
2022-07-25 19:29:23 +0700

    update home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,32 @@
 <template>
-    <h1>Home</h1>
+<div class="">
+    <div
+      id="bannerHome"
+      class="HomeBanner"
+      data-ride="banner"
+    >
+      <div class="banner-inner">
+        <div
+          class="banner-item"
+          v-for="(banner, idx) in banners"
+          :class="{ active: idx == 0 }"
+          :key="idx"
+        >
+          <img :src="banner" alt="" class="img-fluid" style="height: 570px; width: 100%; object-fit: cover;" />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+<script>
+export default {
+  name: 'app-head',
+  data () {
+    return {
+      banners: [
+        'https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80'
+      ]
+    }
+  }
+}
+</script>
