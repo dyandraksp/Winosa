69b0d55015a7a2b3c007bf96ea256a80f2cba21d
dhalaayu23 <dhalaayumarisya90@gmail.com>
2022-07-27 14:02:03 +0700

    update contact

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -49,9 +49,97 @@ Berikut cara untuk menghubungi kami</a>
 
 </div>
 <div class="content">
-<img :src="vector_contact" style="width: 610px; height: 617px; left: 732px; top: 82px; object-fit: cover;">
+<img :src="vector_contact" style="width: 510px; height: 517px; margin-left: 200px;  left: 852px; top: 82px; object-fit: cover;">
 </div>
+<h3 style= " text-align: left; margin-left: 160px; top: 723px;">
+Kirimkan Kami Sebuah Pesan
+</h3>
+<a style="width: 807px;
+height: 42px;
+left: 155px;
+top: 780px;
 
+font-family: Inter, sans-serif ;
+font-style: normal;
+font-weight: 400;
+font-size: 20px;
+line-height: 150%;
+/* or 30px */
+
+text-align: center;
+
+color: #5D5D5D; text-align: left; margin-left: 160px;"> Kirimkan saja permintaan anda dan kami akan menemukan  solusi terbaik untuk anda </a>
+<p></p>
+<form action="/action_page.php">
+  <label for="fname" style="text-align: left; margin-left: 160px;
+
+font-family: Inter, sans-serif;
+font-style: normal;
+font-weight: 400;
+font-size: 20px;
+line-height: 150%;
+/* or 30px */
+
+color: #5D5D5D;
+">Nama</label><br>
+
+  <input type="text" id="fname" name="fname" value="Nama Lengkap" style=" text-align: left; margin-left: 160px;left: 110px; width: 500px; height: 48px;
+right: 24px;top: 780px; bottom: 46.99%; background: #FFFFFF; border: 1px solid rgba(0, 0, 0, 0.5);
+border-radius: 4px;">
+
+  <label for="lname" style="text-align: left; margin-left: 140px; left: 200px;
+right: 33.96%;
+top: 780px;
+bottom: 50.91%;
+
+font-family: Inter, sans-serif;
+font-style: normal;
+font-weight: 400;
+font-size: 20px;
+line-height: 150%;
+/* or 30px */
+
+color: #5D5D5D;
+"> Email</label>
+ <input type="text" id="fname" name="fname" value="Nama Lengkap" style=" text-align: left; margin-left: 800px;left: 200px; width: 500px; height: 48px;
+right: 24px;
+top: 780px;
+bottom: 50.91%;
+
+background: #FFFFFF;
+border: 1px solid rgba(0, 0, 0, 0.5);
+border-radius: 4px;">
+
+<br><br>
+ <label for="fname" style="text-align: left; margin-left: 160px;
+left: 11.04%;
+right: 75.49%;
+top: 888px;
+bottom: 44.14%;
+
+font-family: Inter, sans-serif;
+font-style: normal;
+font-weight: 400;
+font-size: 20px;
+line-height: 150%;
+/* or 30px */
+
+color: #5D5D5D;
+">Subject</label><br>
+
+  <input type="text" id="fname" name="fname" value="Subject" style=" text-align: left; margin-left: 160px;left: 110px; width: 500px; height: 48px;
+right: 24px;
+top: 780px;
+bottom: 46.99%;
+
+background: #FFFFFF;
+border: 1px solid rgba(0, 0, 0, 0.5);
+border-radius: 4px;">
+<br>
+
+  <input type="submit" value="Kirim">
+
+</form>
 </body>
 </html>
 
