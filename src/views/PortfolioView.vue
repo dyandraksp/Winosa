a98c4f808eeac41caf3d936b4c3241d5141ead63
delia vergarefta <deliaverga1001@gmail.com>
2022-08-02 14:01:57 +0700

    update portfolio

---
+++ src/views/PortfolioView.vue
@@ -0,0 +1,32 @@
+<template>
+<div class="">
+    <div
+      id="bannerportfolio"
+      class="PortfolioBanner"
+      data-ride="banner"
+    >
+      <div class="banner-inner">
+        <div
+          class="banner-item"
+          v-for="(banner, idx) in banners"
+          :class="{ active: idx == 0 }"
+          :key="idx"
+        >
+          <img :src="banner" alt="" class="img-fluid" style="height: 570px; width: 100%; object-fit: cover;" />
+        </div>
+    </div>
+    </div>
+</div>
+</template>
+<script>
+export default {
+  name: 'app-head',
+  data () {
+    return {
+      banners: [
+        'https://images.unsplash.com/photo-1432821596592-e2c18b78144f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+      ]
+    }
+  }
+}
+</script>
