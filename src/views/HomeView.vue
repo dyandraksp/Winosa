76c3a871cc561ace8cbbc983bbbb94afe68dacad
dhalaayu23 <dhalaayumarisya90@gmail.com>
2022-08-04 14:33:53 +0700

    update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -74,17 +74,22 @@ color: #5D5D5D;">
   <div class="item1">
     <img :src="img5" style="width:86px; height:75px; object-fit: fill;">
      <h4 style="text-align: center;">Bussines Development</h4> <br/>
-     <p>
+     <p style="height: 149px;
+width: 275px;
+left: 146px;
+top: 1980px;
+border-radius: nullpx;
+">
        1. Create cultural awareness
-
+<br>
 2. Market research and introduction
-
+<br>
 3. Conducting consumer and business insights
-
+<br>
 4. Introduction to local business partners
-
+<br>
 5. Marketing campaign
-
+<br>
 6. Setting up your branch office
 </p>
 
@@ -92,18 +97,54 @@ color: #5D5D5D;">
   <div class="item2">
     <img :src="img6" style="width:86px; height:75px; object-fit: fill;">
      <h4 style="text-align: center;">Outsourcing</h4> <br>
-     <p>
-       We provide offshore services to support you in developing the next generation killer application. Whether you need test analysts, full stack, frontend or backend developers for your web or mobile applications you can partner with us to extend your local development team or fully outsource development and testing to Winosa. Either way, you will have full control over the extended team who will be part of your daily scrums. This means that on a daily basis, you are in charge of everything from work hours to the planning of sprints. During your project, we are here to take care of all the administration, so you can focus on your business.
+     <p style="margin-left: 1px;
+width: 245px;
+height: 149px;
+left: 553px;
+top: 1974px;
+
+font-family: inter, sans-serif;
+font-style: normal;
+font-weight: 400;
+font-size: 16px;
+line-height: 19px;
+letter-spacing: 0.25px;
+
+color: #5D5D5D;
+">
+       We provide offshore services to support you in developing the next generation killer application.
+       <br>Whether you need test analysts, full stack, frontend or backend developers for your web or mobile applications
+       <br>you can partner with us to extend your local development team or fully outsource development and testing to Winosa.
+       <br>Either way, you will have full control over the extended team who will be part of your daily scrums.
+       <br>This means that on a daily basis, you are in charge of everything from work hours to the planning of sprints.
+       <br>During your project, we are here to take care of all the administration, so you can focus on your business.
       </p>
   </div>
   <div class="item3">
     <img :src="img7" style="width:86px; height:75px; object-fit: fill;">
      <h4 style="text-align: center;">Legal</h4> <br>
-     <p>
+     <p style="
+width: 245px;
+height: 149px;
+left: 955px;
+top: 1980px;
+
+font-family: inter,sans-serif;
+font-style: normal;
+font-weight: 400;
+font-size: 16px;
+line-height: 19px;
+letter-spacing: 0.25px;
+
+color: #5D5D5D;
+">
        Once you decide to enter the Indonesian market our Legal advisors will guide you through all the necessary legal, bureaucratic and tax-office requirements. Our Legal officers offer a one-shop all-in-one service so you can focus on your business.
       </p>
   </div>
 </div>
+<p></p>
+<p></p>
+<p></p>
 <div class="grid-container" style="grid-gap: 0px;">
 <div class="item1">
   <img src="../assets/contact_home.jpg" style="width: 431px; height: 431px;"/>
@@ -153,7 +194,7 @@ export default {
 .grid-container {
   display: grid;
   grid-template-columns: auto auto auto auto;
-  grid-gap: 100px;
+  grid-gap: 10px;
   padding: 10px;
   padding-bottom: 64px;
   border:0px;
